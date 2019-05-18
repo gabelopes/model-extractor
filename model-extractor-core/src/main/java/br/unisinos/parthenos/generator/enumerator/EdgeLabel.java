eb355f33c5c9f06de069d3e078a20dc1cd94323c
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-05-18 03:11:04 -0300

    Improves Enumerators interfaces

--- model-extractor-core/src/main/java/br/unisinos/parthenos/generator/enumerator/EdgeLabel.java
+++ model-extractor-core/src/main/java/br/unisinos/parthenos/generator/enumerator/EdgeLabel.java
@@ -1,5 +1,16 @@
 package br.unisinos.parthenos.generator.enumerator;
 
+import br.unisinos.parthenos.generator.prolog.term.Atom;
 import br.unisinos.parthenos.generator.prolog.term.Term;
 
-public interface EdgeLabel extends Term<String> {}
+public interface EdgeLabel extends Enumerator, Term<String> {
+  @Override
+  default String getContent() {
+    return this.name();
+  }
+
+  @Override
+  default String getTerm() {
+    return new Atom(this.getContent().toLowerCase()).getTerm();
+  }
+}
---
+++ model-extractor-core/src/main/java/br/unisinos/parthenos/generator/enumerator/Enumerator.java
@@ -0,0 +1,6 @@
+package br.unisinos.parthenos.generator.enumerator;
+
+public interface Enumerator {
+  String name();
+  int ordinal();
+}
